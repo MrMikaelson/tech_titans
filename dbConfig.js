666420a1d9a2c3850efd285a5314d392ae70117a
MrMikaelson <120541629+MrMikaelson@users.noreply.github.com>
2023-08-02 17:35:52 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -7,7 +7,10 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgres://tech_jf4z_user:VYA0pWI7MjvngdZHvqimWYwq43lFXjuD@dpg-cj542uk5kgrc7389m7ig-a/tech_jf4z`;
 
 const pool = new Pool({
-    connectionString: connectionString
+    connectionString: connectionString,
+    ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = { pool };
